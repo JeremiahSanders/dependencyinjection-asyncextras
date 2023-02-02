da07abac0c04e6e03244643b1d914a8afe51618a
Jeremiah Sanders <1328897+JeremiahSanders@users.noreply.github.com>
2023-02-01 23:46:56 -0600

    task: apply style

--- src/library/ServiceCollectionAsyncExtensions.cs
+++ src/library/ServiceCollectionAsyncExtensions.cs
@@ -9,40 +9,40 @@ namespace Jds.DependencyInjection.AsyncExtras;
 public static class ServiceCollectionExtensions
 {
   /// <summary>
-  ///   Adds a <see cref="ServiceDescriptor" /> for an <see cref="IAsyncResolver{T}" /> of <typeparamref name="T" /> if that
-  ///   type has not been registered.
+  ///   Adds a <see cref="ServiceDescriptor" /> for an <see cref="IAsyncResolver{T}" /> of <typeparamref name="TService" />
+  ///   if that type has not been registered.
   /// </summary>
   /// <remarks>
   ///   <para>
-  ///     When requiring this async dependency (<typeparamref name="T" />) via constructor parameters, make sure to use an
-  ///     <see cref="IAsyncResolver{T}" /> of <typeparamref name="T" />.
+  ///     When requiring this async dependency (<typeparamref name="TService" />) via constructor parameters, make sure to
+  ///     use an <see cref="IAsyncResolver{T}" /> of <typeparamref name="TService" />.
   ///   </para>
   ///   <para>
-  ///     This registration does not enable <typeparamref name="T" /> to be resolved from <see cref="IServiceProvider" />
-  ///     instances created from this <see cref="IServiceCollection" />.
+  ///     This registration does not enable <typeparamref name="TService" /> to be resolved from
+  ///     <see cref="IServiceProvider" /> instances created from this <see cref="IServiceCollection" />.
   ///   </para>
   ///   <para>
   ///     Alternately, use the <see cref="ServiceProviderAsyncExtensions.GetServiceAsync{T}" /> and
   ///     <see cref="ServiceProviderAsyncExtensions.GetRequiredServiceAsync{T}" /> methods to access
-  ///     <typeparamref name="T" /> (from any <see cref="IServiceProvider" /> instances created from this
+  ///     <typeparamref name="TService" /> (from any <see cref="IServiceProvider" /> instances created from this
   ///     <see cref="IServiceCollection" />).
   ///   </para>
   /// </remarks>
-  /// <param name="serviceCollection"></param>
-  /// <param name="asyncFactory"></param>
-  /// <param name="lifetime"></param>
-  /// <typeparam name="T"></typeparam>
-  /// <returns></returns>
-  public static IServiceCollection TryAddAsyncResolver<T>(
+  /// <param name="serviceCollection">This <see cref="IServiceCollection" /> instance.</param>
+  /// <param name="asyncFactory">A factory method which asynchronously creates <typeparamref name="TService" /> instances.</param>
+  /// <param name="lifetime">A <see cref="ServiceLifetime" />.</param>
+  /// <typeparam name="TService">The type of the service to add.</typeparam>
+  /// <returns>This <see cref="IServiceCollection" />.</returns>
+  public static IServiceCollection TryAddAsyncResolver<TService>(
     this IServiceCollection serviceCollection,
-    Func<IServiceProvider, Task<T>> asyncFactory,
+    Func<IServiceProvider, Task<TService>> asyncFactory,
     ServiceLifetime lifetime = ServiceLifetime.Transient
   )
   {
     serviceCollection.TryAdd(
       new ServiceDescriptor(
-        typeof(IAsyncResolver<T>),
-        serviceProvider => new AsyncResolver<T>(serviceProvider, asyncFactory),
+        typeof(IAsyncResolver<TService>),
+        serviceProvider => new AsyncResolver<TService>(serviceProvider, asyncFactory),
         lifetime
       )
     );
